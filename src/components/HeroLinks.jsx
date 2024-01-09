dd4265a97f0e2d9c5a208c56057f62c92bbfa453
DENDUlean <denis.stoklas@seznam.cz>
2024-01-09 14:05:04 +0100

    uprava block/hidden pro HeroLinks podle zařízení

--- src/components/HeroLinks.jsx
+++ src/components/HeroLinks.jsx
@@ -8,18 +8,18 @@ const HeroLinks = () => {
     return (
         <div className="lg:flex lg:flex-col lg:gap-2 lg:w-6/12 sm:flex sm:flex-col sm:text-start sm:items-start sm:w-4/12 sm:gap-3 flex gap-3 text-sm w-9/12 text-center justify-center">
             <div className="lg:flex lg:items-start lg:flex-row lg:w-full sm:w-full sm:items-start sm:flex sm:flex-row flex flex-col items-center gap-2">
-                <FaGithub size={20} className="text-gray-500"/>
-                <Link to="https://github.com/DENDUlean" className="text-gray-500 hover:text-black">Github Profil</Link>
+                <Link to="https://github.com/DENDUlean" className="cursor-pointer"><FaGithub size={20} className="text-gray-500"/></Link>
+                <Link to="https://github.com/DENDUlean" className="text-gray-500 hover:text-black sm:block hidden">Github Profil</Link>
             </div>
 
             <div className="lg:flex lg:items-start lg:flex-row sm:w-full sm:items-start sm:flex sm:flex-row flex flex-col items-center gap-2">
-                <FaLinkedin size={20} className="text-gray-500"/>
-                <Link to="https://github.com/DENDUlean" className="text-gray-500 hover:text-black">Linkedln Profil</Link>
+                <Link to="www.linkedin.com/in/denis-stoklasa-734723281" className="cursor-pointer"><FaLinkedin size={20} className="text-gray-500"/></Link>
+                <Link to="www.linkedin.com/in/denis-stoklasa-734723281" className="text-gray-500 hover:text-black sm:block hidden">Linkedln Profil</Link>
             </div>
 
             <div className="lg:flex lg:items-start lg:flex-row sm:w-full sm:items-start sm:flex sm:flex-row flex flex-col items-center gap-2">
-                <FaInstagramSquare size={20} className="text-gray-500"/>
-                <Link to="https://www.instagram.com/denis.stoklasa/" className="text-gray-500 hover:text-black">Instagram Profil</Link>
+                <Link to="https://www.instagram.com/denis.stoklasa/" className="cursor-pointer"><FaInstagramSquare size={20} className="text-gray-500"/></Link>
+                <Link to="https://www.instagram.com/denis.stoklasa/" className="text-gray-500 hover:text-black sm:block hidden">Instagram Profil</Link>
             </div>
         </div>
     )

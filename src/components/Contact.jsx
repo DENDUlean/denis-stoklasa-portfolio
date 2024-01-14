bf03d343662733f62d067c7bd34e993c2d0fc51e
DENDUlean <denis.stoklas@seznam.cz>
2024-01-14 23:59:13 +0100

    úprava stack sekce a kontakt formuláře

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,23 @@
 const Contact = () => {
     return (
-        <div className="flex flex-col gap-4 lg:w-4/12 w-9/12 text-gray-500 dark:text-white">
+        <div className="flex flex-col gap-4 lg:w-4/12 w-9/12">
 
-            <p className="lg:text-base text-xs">Pokud jsem Vás zaujal můžete mi zaslat e-mail</p>
+            <p className="lg:text-base text-xs text-black dark:text-white">Pokud jsem Vás zaujal můžete mi zaslat e-mail</p>
             
             <div className="flex flex-col justify-center items-center">
                 <form action={import.meta.env.REACT_APP_FORM_KEY} method="POST" className="flex flex-col w-full">
                     <input type="text" name="name" placeholder="Vaše jméno..." required
-                        className="focus:outline-none lg:text-base text-xs p-2 mb-4 border-2 bg-transparent border-gray-300 rounded-md w-64"
+                        className="focus:outline-none lg:text-base text-xs p-2 mb-4 border-2 bg-transparent border-gray-400 rounded-md w-64"
                     />
 
                     <input type="hidden" name="_gotcha" className="hidden"/>
                 
                     <input type="email" name="email" placeholder="Váš e-mail..." required
-                        className="focus:outline-none lg:text-base text-xs p-2 mb-4 border-2 bg-transparent border-gray-300 rounded-md w-64"
+                        className="focus:outline-none lg:text-base text-xs p-2 mb-4 border-2 bg-transparent border-gray-400 rounded-md w-64"
                     />
 
                     <textarea name="message" placeholder="Vaše zpráva...(max 150 znaků)" cols="30" rows="5" required maxlength="150"
-                        className="p-2 lg:text-base text-xs mb-4 bg-transparent border-gray-300 border-2 lg:w-8/12 w-64 rounded-md focus:outline-none"
+                        className="p-2 lg:text-base text-xs mb-4 bg-transparent border-gray-400 border-2 lg:w-8/12 w-64 rounded-md focus:outline-none"
                     >
                     </textarea>
 
